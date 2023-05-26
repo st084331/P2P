cd2d2b7252f7d5bccd7e2454aebc7dc9a052e94c
st084331 <st084331@student.spbu.ru>
2023-05-26 04:51:28 +0300

    Добавьте файлы проекта.

---
+++ P2P_Tests/ChatTests.cs
@@ -0,0 +1,139 @@
+
+using System.Net;
+using P2P.Chat;
+
+namespace P2P_Tests
+{
+	public class ChatTests
+	{
+		[Test]
+		public void ChatCreate()
+		{
+			var chat = new Chat(5005);
+			chat.Dispose();
+		}
+
+		[Test]
+		public void TwoChatsInteract()
+		{
+			var messageReceived = 0;
+
+			var chat5000 = new Chat(5000);
+			var chat5001 = new Chat(5001);
+			chat5001.ConnectTo(IPEndPoint.Parse("127.0.0.1:5000"));
+
+			chat5000.OnMessage += _ => messageReceived++;
+			chat5001.OnMessage += _ => messageReceived++;
+
+			Thread.Sleep(100);
+
+			chat5000.Send("A");
+
+			Thread.Sleep(100);
+
+			chat5001.Send("B");
+
+			Thread.Sleep(100);
+
+			Assert.That(4, Is.EqualTo(messageReceived));
+
+			chat5000.Dispose();
+			chat5001.Dispose();
+		}
+
+		[Test]
+		public void ThreeChatsInteract()
+		{
+			Interact(3, new List<int>());
+		}
+
+		[Test]
+		public void FourChatsInteract()
+		{
+			Interact(4, new List<int>());
+		}
+
+		[Test]
+		public void FiveChatsInteract()
+		{
+			Interact(5, new List<int>());
+		}
+
+		[Test]
+		public void ThreeChatsInteractDestruction()
+		{
+			Interact(3, new List<int> { 1, 2 });
+		}
+
+		[Test]
+		public void FourChatsInteractDestruction()
+		{
+			Interact(4, new List<int> { 2, 0 });
+		}
+
+		[Test]
+		public void FiveChatsInteractDestruction()
+		{
+			Interact(5, new List<int> { 1, 3, 4 });
+		}
+
+		private void Interact(int chatCount, List<int> disposeSeq)
+		{
+			var messageReceived = 0;
+
+			var chats = new List<Chat>();
+
+			for (var i = 0; i < chatCount; i++)
+			{
+				chats.Add(new Chat(5003 + i));
+				Thread.Sleep(100);
+			}
+
+			foreach (var chat in chats)
+			{
+				chat.OnMessage += _ => messageReceived++;
+
+				if (chat == chats.FirstOrDefault())
+					continue;
+
+				chat.ConnectTo(IPEndPoint.Parse("127.0.0.1:5003"));
+				Thread.Sleep(100);
+			}
+
+			foreach (var elem in disposeSeq)
+			{
+				chats[elem].Dispose();
+
+				Thread.Sleep(100);
+			}
+
+			var index = 0;
+
+			foreach (var chat in chats)
+			{
+				if (disposeSeq.Contains(index++))
+					continue;
+
+				chat.Send(Convert.ToString(index));
+
+				Thread.Sleep(100);
+			}
+
+			chatCount -= disposeSeq.Count;
+
+			Assert.That(chatCount * chatCount, Is.EqualTo(messageReceived));
+
+			index = 0;
+
+			foreach (var chat in chats)
+			{
+				if (disposeSeq.Contains(index++))
+					continue;
+
+				chat.Dispose();
+			}
+
+			Thread.Sleep(100);
+		}
+	}
+}
